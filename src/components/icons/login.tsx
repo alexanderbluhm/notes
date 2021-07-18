ba17e37a4dec6139710ee75634343b401057871b
Alexander Bluhm <lamiy-develop@live.de>
2021-07-18 20:31:10 +0200

    Fixed svg attr error

--- src/components/icons/login.tsx
+++ src/components/icons/login.tsx
@@ -5,22 +5,22 @@ export const LoginIcon = (props) => {
     <svg {...props} fill="none" viewBox="0 0 24 24">
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth="1"
         d="M9.75 8.75L13.25 12L9.75 15.25"
       ></path>
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth="1"
         d="M9.75 4.75H17.25C18.3546 4.75 19.25 5.64543 19.25 6.75V17.25C19.25 18.3546 18.3546 19.25 17.25 19.25H9.75"
       ></path>
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth="1"
         d="M13 12H4.75"
       ></path>
