a5a565ceb1397e5f65f595c7ae73f77524359c8b
Alexander Bluhm <lamiy-develop@live.de>
2021-07-18 23:17:48 +0200

    Update save behavior

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export * from './bookmark';
 export * from './trash';
 export * from './login';
 export * from './notelist';
+export * from './lock';
--- src/pages/notes/[id]/index.tsx
+++ src/pages/notes/[id]/index.tsx
@@ -7,7 +7,7 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
 import { formatRelative, parseISO } from "date-fns";
-import { BookmarkIcon, TrashIcon } from "@/components/icons";
+import { BookmarkIcon, LockIcon, TrashIcon } from "@/components/icons";
 import { Menu } from "@headlessui/react";
 import { useNotification } from "@/lib/useNotification";
 
@@ -49,21 +49,16 @@ const Index = (props: Props) => {
     router.replace("/");
   };
 
-  const handleUpdate = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key == "Enter" && !e.shiftKey) {
-      // we want to submit on enter and don't create a new line
-      e.preventDefault();
+  const handleUpdate = async () => {
+    // if nothing changed we don't want an update
+    if (note.content === content) return;
 
-      // if nothing changed we don't want an update
-      if (note.content === content) return;
+    setPreviewActive(true);
 
-      setPreviewActive(true);
+    let updatedNote = { ...note };
+    updatedNote.content = content;
 
-      let updatedNote = { ...note };
-      updatedNote.content = content;
-
-      await updateNote(updatedNote);
-    }
+    await updateNote(updatedNote);
   };
 
   const updateNote = async (note) => {
@@ -77,6 +72,8 @@ const Index = (props: Props) => {
     }).catch((ex) => {
       console.error(ex);
     });
+
+    addNotification({ text: "Updated Note", type: "info" });
   };
 
   return (
@@ -106,6 +103,14 @@ const Index = (props: Props) => {
                   stroke={note.bookmarked ? "transparent" : "url(#grad1)"}
                 />
               </button>
+              <button className="inline-flex p-1.5 rounded-lg hover:bg-gray-800 transition-colors duration-200">
+                <span className="sr-only">Managae visibility</span>
+                <LockIcon
+                  aria-hidden="true"
+                  className="w-6 h-6 from-green-400 to-green-500"
+                  stroke={"url(#grad3)"}
+                />
+              </button>
               <DeleteDialog onDelete={handleDelete}>
                 <Menu.Button className="inline-flex p-1.5 rounded-lg hover:bg-gray-800 transition-colors duration-200">
                   <span className="sr-only">Delete note</span>
@@ -116,6 +121,16 @@ const Index = (props: Props) => {
                   />
                 </Menu.Button>
               </DeleteDialog>
+
+              {/* Seperator */}
+              <div className="w-px h-6 bg-gray-700 ml-6"></div>
+              <button
+                onClick={handleUpdate}
+                style={{ zIndex: 10 }}
+                className="hover:bg-gray-800 border border-transparent transition-colors isolate px-3 py-1.5 bg-black rounded-md text-sm backdrop-filter backdrop-blur-md"
+              >
+                Save
+              </button>
             </div>
           </div>
 
@@ -139,24 +154,25 @@ const Index = (props: Props) => {
 
               {!previewActive && (
                 <textarea
-                  onKeyDown={handleUpdate}
                   rows={5}
                   placeholder="Content ..."
                   value={content}
                   onChange={(e) => setContent(e.target.value)}
-                  className="bg-gray-900 placeholder-gray-500 isolate flex justify-between items-center px-3 py-2 w-full rounded-md"
+                  className="bg-gray-900 bg-opacity-70 placeholder-gray-500 isolate flex justify-between items-center px-3 py-2 w-full rounded-md"
                 >
                   {note?.content}
                 </textarea>
               )}
 
-              <button
-                onClick={() => setPreviewActive((prev) => !prev)}
-                style={{ zIndex: 10 }}
-                className="absolute hover:bg-gray-800 border border-gray-700 transition-colors isolate top-2 right-2 px-3 py-1.5 bg-black rounded-md text-sm bg-opacity-40 backdrop-filter backdrop-blur-md"
-              >
-                {previewActive ? "Edit" : "Preview"}
-              </button>
+              <div className="absolute top-2 right-2 flex items-center justify-between space-x-2">
+                <button
+                  onClick={() => setPreviewActive((prev) => !prev)}
+                  style={{ zIndex: 10 }}
+                  className="hover:bg-gray-800 border border-gray-700 transition-colors isolate px-3 py-1.5 bg-black rounded-md text-sm bg-opacity-40 backdrop-filter backdrop-blur-md"
+                >
+                  {previewActive ? "Edit" : "Preview"}
+                </button>
+              </div>
             </div>
           </div>
         </>
